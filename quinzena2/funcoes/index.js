d08151f92af32e23585a9d2d1264544e809f6b0b
AP Leite <thaispasottileite@gmail.com>
2021-09-16 18:50:18 -0300

    subindo-exercicios-funcoes

---
+++ quinzena2/funcoes/index.js
@@ -0,0 +1,103 @@
+//                      Exercícios de interpretação de código
+//      Exercício 1
+//  a) Será impresso no console os valores: 10 e 50.
+//  b) Se retirássemos os dois console.log nada apareceria no console, pois não foi dado nenhum comando para tal.
+
+//      Exercício 2
+//  a) Essa função recebe um texto do usuário, transforma em caixa baixa e verifica se há a palavra "cenoura" no corpo do texto.
+//  e retorna um True or False para cada ocorrência.
+//  b) Para "Eu gosto de cenoura" a saída será True, para "CENOURA é bom pra vista" será True e para "Cenouras crescem na terra" também será True.
+
+/*
+//                      Exercícios de escrita de código
+//      Exercício 1
+//  a)
+
+function imprimeMensagem() {
+    console.log("Eu sou Thais, tenho 26 anos, moro em São Paulo e sou estudante")
+}
+console.log(imprimeMensagem())      
+
+//  b)
+function imprimeSaudacoes(nome, idade, cidade, profissao) {
+    nome = "Thais"
+    idade = Number(26)
+    cidade = "São Paulo"
+    profissao = "estudante"
+    const mensagemUnificada = `Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`
+    return mensagemUnificada
+}
+console.log(imprimeSaudacoes())
+
+//      Exercício 2
+//  a)
+
+function somaDoisNumeros(num1, num2) {
+    const soma = num1 + num2
+    return soma
+}
+const resultado = somaDoisNumeros(5, 10)
+console.log(resultado)
+
+//  b)
+function verificaNumeroMaior(numero1, numero2) {
+    const checaNumeroMaior = numero1 >= numero2
+    return checaNumeroMaior
+}
+const numeroMaior = verificaNumeroMaior(5, 9)
+console.log(numeroMaior)
+
+//  c) 
+
+function verificaPar(num1) {
+    const verificaNumeroPar = num1 % 2 === 0
+    return verificaNumeroPar
+}
+const resultadoNumeroPar = verificaPar(7)
+console.log(resultadoNumeroPar)
+
+//  d)  
+function escreveMensagem(){
+const texto = prompt("Digite uma frase")
+console.log(texto.toLowerCase(), texto.length)
+}
+
+escreverMsg()
+
+//      Exercício 3
+
+
+const numeroUsuario = Number(prompt("Insira um número:"))
+const numeroUsuario2 = Number(prompt("Insira outro número:"))
+
+function somaNumeros(numeroUsuario, numeroUsuario2) {
+    const soma = numeroUsuario + numeroUsuario2
+    return soma
+}
+const resultadoSoma = somaNumeros(numeroUsuario, numeroUsuario2)
+console.log(resultadoSoma)
+
+function subtraiNumeros(numeroUsuario, numeroUsuario2) {
+    const subtrai = numeroUsuario - numeroUsuario2
+    return subtrai
+}
+const resultadoSubtracao = subtraiNumeros(numeroUsuario, numeroUsuario2)
+console.log(resultadoSubtracao)
+
+function multiplicaNumeros(numeroUsuario, numeroUsuario2) {
+    const multiplica = numeroUsuario * numeroUsuario2
+    return multiplica
+}
+const resultadoMultiplicacao = multiplicaNumeros(numeroUsuario, numeroUsuario2)
+console.log(resultadoMultiplicacao)
+
+function divideNumeros(numeroUsuario, numeroUsuario2) {
+    const divide = numeroUsuario / numeroUsuario2
+    return divide
+}
+const resultadoDivisao = divideNumeros(numeroUsuario, numeroUsuario2)
+console.log(resultadoDivisao)
+
+*/
+
+
